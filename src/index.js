d0271eb090121b96fd92c16d7586f5156f47ffa6
Minh Mai <minhmmai@gmail.com>
2020-04-09 10:55:54 +1000

    change MUI theme colours

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
 
+import theme from './assets/theme/boulder-pizza-cows';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const app = (
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 )
 
