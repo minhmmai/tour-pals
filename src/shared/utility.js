60ee6d7b35f1aa3b0cc3eaded5d8d87d9163e075
Minh Mai <minhmmai@gmail.com>
2020-04-21 22:13:22 +1000

    add validity checking function

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -10,10 +10,44 @@ export const getForm = (formName) => {
 };
 
 
-export const getFormSections = (formObj) => {
-    const sections = [];
-    formObj.sections.forEach((section) => {
-      sections.push(section);
-    });
-    return sections;
-  };
+export const validateField = (fields, field, rules) => {
+    let isValid = true;
+    if (!rules) {
+        return true;
+    }
+
+    if (rules.isRequired) {
+        isValid = field.value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = field.value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+        isValid = field.value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(field.value) && isValid
+    }
+
+    if (rules.minValue) {
+        if (typeof(rules.minValue) === 'string') {
+            isValid = field.value >= fields[rules.minValue].value && isValid
+        }else {
+            isValid = field.value >= rules.minValue && isValid
+        }
+    }
+
+    if (rules.maxValue) {
+       if (typeof(rules.maxValue) === 'string') {
+            isValid = field.value < fields[rules.maxValue].value && isValid
+        }else {
+            isValid = field.value < rules.maxValue && isValid
+        }
+    }
+
+    return isValid;
+}
