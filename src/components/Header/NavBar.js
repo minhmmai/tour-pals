b0b5178f0fd6a3fc866861aa0d27ac805d7f23f3
Minh Mai <minhmmai@gmail.com>
2020-05-07 23:33:05 +1000

    add menu toggle button for phone

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -3,15 +3,17 @@ import React from 'react';
 import NavItems from './NavItems';
 import Button from '../UI/Button/Button';
 import Logo from '../Logo/Logo';
+import ToggleButton from "./ToggleButton";
 import classes from './NavBar.module.scss';
 
 const NavBar = props => {
     return (
         <header>
             <nav className={classes.NavBar}>
-                <Logo link="/"/>
-                <NavItems />
-                <Button type="cta">Book Now!</Button>
+                    <Logo link="/" />
+                    <NavItems />
+                    <Button type="cta">Book Now!</Button>
+                    <ToggleButton />
             </nav>
         </header>
     )
---
+++ src/components/Header/ToggleButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+
+import classes from "./ToggleButton.module.scss";
+
+const ToggleButton = props => {
+
+    return (
+        <div className={classes.ToggleButton}>
+            <div></div>
+        </div >
+    )
+};
+
+export default ToggleButton;
