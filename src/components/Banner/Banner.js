8f03b5c5db65e0130b7ee887f6aab26e47b2c37b
Minh Mai <minhmmai@gmail.com>
2020-05-07 12:48:07 +1000

    add responsiveness for navigation bar

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,6 +5,7 @@ import classes from './Banner.module.scss';
 const Banner = props => {
     return (
         <section className={classes.Banner}>
+            <h1>Summer Time!</h1>
             
         </section>
     )
