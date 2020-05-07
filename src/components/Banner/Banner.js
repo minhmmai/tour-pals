f36035f978774056e5f011a29cb489cc989df323
Minh Mai <minhmmai@gmail.com>
2020-05-07 15:52:35 +1000

    responsive banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import classes from './Banner.module.scss';
+import Button from "../UI/Button/Button";
 
 const Banner = props => {
     return (
         <section className={classes.Banner}>
-            <h1>Summer Time!</h1>
-            
+            <div className={classes.Intro}>
+                <h1 className={classes.IntroTitle}>
+                    It's Summer Time
+                </h1>
+                <h3 className={classes.IntroSubTitle}>
+                    Book your tour now to enjoy the best sunshine with us!
+                </h3>
+                <Button type="intro">See Destinations</Button>
+            </div>
         </section>
     )
 };
