e3390f41b2a1bc156d40dd7438312679b5b5a24e
Minh Mai <minhmmai@gmail.com>
2020-05-08 16:03:35 +1000

    add dropdown items for mobile screen

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -5,16 +5,22 @@ import Button from '../UI/Button/Button';
 import Logo from '../Logo/Logo';
 import ToggleButton from "../Header/ToggleButton";
 import classes from './NavBar.module.scss';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const NavBar = props => {
+    const [isMobile, setIsMobile] = useState(false);
 
+    useEffect(() =>{
+        setIsMobile(window.innerWidth <= 600);
+    }, [])
     return (
         <header>
             <nav className={classes.NavBar}>
                 <Logo link="/" />
                 <Button type="cta">Book Now!</Button>
-                <ToggleButton/>
-                <NavItems />
+                <ToggleButton />
+                <NavItems isMobile={isMobile}/>              
             </nav>
         </header>
     )
--- src/components/Header/NavItems.js
+++ src/components/Header/NavItems.js
@@ -4,19 +4,42 @@ import NavItem from './NavItem';
 import classes from './NavItems.module.scss';
 import Dropdown from "./Dropdown";
 import DropdownItem from "./DropdownItem";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
-const NavItems = () => {
+const NavItems = ({ isMobile }) => {
+    let navItems = "";
+
+    !isMobile
+        ? navItems = (
+            <ul className={classes.NavItems}>
+                <NavItem link="/" exact="true">Home</NavItem>
+                <NavItem link="/destinations">Destinations</NavItem>
+                <NavItem link="/services">Services</NavItem>
+                <Dropdown label="My Trips">
+                    <DropdownItem link="/manage-booking">Manage Booking</DropdownItem>
+                    <DropdownItem link="/enquire">Enquire</DropdownItem>
+                </Dropdown>
+                <NavItem link="/about-us">About Us</NavItem>
+            </ul>
+        ) : navItems = (
+            <ul className={classes.NavItems}>
+                <NavItem link="/" exact="true">Home</NavItem>
+                <NavItem link="/destinations">Destinations</NavItem>
+                <NavItem link="/services">Services</NavItem>
+                <div className={classes.Dropdown}>
+                    <NavItem link="">My Trips <FontAwesomeIcon icon={faCaretDown} /></NavItem>
+                    <div className={classes.DropdownItem}>
+                        <NavItem link="/manage-booking">Manage Booking</NavItem>
+                        <NavItem link="/enquire">Enquire</NavItem>
+                    </div>
+                </div>
+                <NavItem link="/about-us">About Us</NavItem>
+            </ul>
+        )
+
     return (
-        <ul className={classes.NavItems}>
-            <NavItem link="/" exact="true">Home</NavItem>
-            <NavItem link="/destinations">Destinations</NavItem>
-            <NavItem link="/services">Services</NavItem>
-            <Dropdown label="My Trips">
-                <DropdownItem link="/manage-booking">Manage Booking</DropdownItem>
-                <DropdownItem link="/enquire">Enquire</DropdownItem>
-            </Dropdown>
-            <NavItem link="/about-us">About Us</NavItem>
-        </ul>
+        navItems
     )
 };
 
