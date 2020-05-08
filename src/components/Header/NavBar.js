6fec4cd8f289371abada0a1e74c1fad4a278a12f
Minh Mai <minhmmai@gmail.com>
2020-05-08 15:09:48 +1000

    add mobile menu

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -3,17 +3,18 @@ import React from 'react';
 import NavItems from './NavItems';
 import Button from '../UI/Button/Button';
 import Logo from '../Logo/Logo';
-import ToggleButton from "./ToggleButton";
+import ToggleButton from "../Header/ToggleButton";
 import classes from './NavBar.module.scss';
 
 const NavBar = props => {
+
     return (
         <header>
             <nav className={classes.NavBar}>
-                    <Logo link="/" />
-                    <NavItems />
-                    <Button type="cta">Book Now!</Button>
-                    <ToggleButton />
+                <Logo link="/" />
+                <Button type="cta">Book Now!</Button>
+                <ToggleButton/>
+                <NavItems />
             </nav>
         </header>
     )
