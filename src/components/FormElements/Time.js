296972a9b558923e322beb9e39a57f9de4188573
Minh Mai <minhmmai@gmail.com>
2020-06-25 21:25:14 +1000

    add time field component

---
+++ src/components/FormElements/Time.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Tooltip from './Tooltip';
+import classes from './Field.module.scss';
+
+const Time = (props) => {
+  const { description, error, handleChange, name, optional, label, isShown, isValid, isTouched, tooltip, type, value } = props
+  return (
+    <div className={[classes.Field, !isShown && classes.Hidden].join(" ")}>
+      <div className={classes.Label}>
+        <label htmlFor={name}>{label}</label>
+        {optional && <span>(optional)</span>}
+        {tooltip && <Tooltip content={tooltip} />}
+      </div>
+      <div className={classes.Date}>
+        <input id={name} name={name} type="time" onChange={handleChange} value={value} />
+      </div>
+      {isShown && isValid && isTouched && <p className={classes.Error}>{error}</p>}
+      {description && <p className={classes.Description}>{description}</p>}
+    </div>
+  );
+};
+
+export default Time;
