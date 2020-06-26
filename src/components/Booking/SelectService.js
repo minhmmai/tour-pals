9343b6afef2e262bb735f72c45204f2cc65cad9c
Minh Mai <minhmmai@gmail.com>
2020-06-26 14:23:22 +1000

    color change on service tile hovered

--- src/components/Booking/SelectService.js
+++ src/components/Booking/SelectService.js
@@ -7,44 +7,44 @@ import classes from "./SelectService.module.scss";
 
 const SelectService = props => {
     return (
-            <section className={classes.Services}>
-                <div className={classes.Airport} onClick={() => props.history.push("booking/airport")}>
-                    <img className={classes.Icon} src={require("../../assets/icon/airport.svg")} alt="Airport icon" />
-                    <div className={classes.ServiceInfo}>
-                        <p className={classes.ServiceDesc}>
-                            We know your flights" departure, landing time. We know if they have been delayed or cancelled too.
+        <section className={classes.Services}>
+            <div className={classes.Airport} onClick={() => props.history.push("booking/airport")}>
+                <img className={classes.Icon} src={require("../../assets/icon/airport.svg")} alt="Airport icon" />
+                <div className={classes.ServiceInfo}>
+                    <p className={classes.ServiceDesc}>
+                        We know your flights" departure, landing time. We know if they have been delayed or cancelled too.
                         </p>
-                        <h4 className={classes.ServiceName}>
-                            Airport Transfer &nbsp;
+                    <h4 className={classes.ServiceName}>
+                        Airport Transfer &nbsp;
                         <FontAwesomeIcon icon={faArrowRight} />
-                        </h4>
-                    </div>
+                    </h4>
                 </div>
-                <div className={classes.Tour} onClick={() => props.history.push("booking/tour")}>
-                    <img className={classes.Icon} src={require("../../assets/icon/travel-map.svg")} alt="Airport icon" />
-                    <div className={classes.ServiceInfo}>
-                        <p className={classes.ServiceDesc}>
-                            Our experienced guides will be there for you all the way. Making sure you"ll get the best experience.
+            </div>
+            <div className={classes.Tour} onClick={() => props.history.push("booking/tour")}>
+                <img className={classes.Icon} src={require("../../assets/icon/travel-map.svg")} alt="Airport icon" />
+                <div className={classes.ServiceInfo}>
+                    <p className={classes.ServiceDesc}>
+                        Our experienced guides will be there for you all the way. Making sure you"ll get the best experience.
                         </p>
-                        <h4 className={classes.ServiceName}>
-                            Private Tours &nbsp;
+                    <h4 className={classes.ServiceName}>
+                        Private Tours &nbsp;
                         <FontAwesomeIcon icon={faArrowRight} />
-                        </h4>
-                    </div>
+                    </h4>
                 </div>
-                <div className={classes.Hourly} onClick={() => props.history.push("booking/hourly")}>
-                    <img className={classes.Icon} src={require("../../assets/icon/speed.svg")} alt="Airport icon" />
-                    <div className={classes.ServiceInfo}>
-                        <p className={classes.ServiceDesc}>
-                            Checkout early? Or just simply have some time in hand? Call us for a mini-tour that suits your schedule.
+            </div>
+            <div className={classes.Hourly} onClick={() => props.history.push("booking/hourly")}>
+                <img className={classes.Icon} src={require("../../assets/icon/speed.svg")} alt="Airport icon" />
+                <div className={classes.ServiceInfo}>
+                    <p className={classes.ServiceDesc}>
+                        Checkout early? Or simply have some time in hand? Call us for a mini-tour that suits your schedule.
                         </p>
-                        <h4 className={classes.ServiceName}>
-                            By The Hour &nbsp;
+                    <h4 className={classes.ServiceName}>
+                        By The Hour &nbsp;
                         <FontAwesomeIcon icon={faArrowRight} />
-                        </h4>
-                    </div>
+                    </h4>
                 </div>
-            </section>
+            </div>
+        </section>
     )
 };
 
